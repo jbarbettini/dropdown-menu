8f42455fa332c591b39e4963216e6df99091eb65
Jennifer Ong <jenn.r.ong@gmail.com>
2016-09-14 12:19:32 -0700

    Add search functionality

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,45 +1,61 @@
 $(document).ready(function() {
   var hosts = [];
 
+  function printHosts(hosts) {
+    $.each(hosts, function(index, host) {
+      $("#host-options")
+        .append('<label><input class="host" type="checkbox" value=' + host + '>  ' + host + '</label> ');
+    })
+  }
+
   $.ajax({
     url: '/get-hosts',
     dataType: 'json',
     success: function(response) {
-      console.log('this is the list of hosts available', response);
       hosts = response.map(function(host) {
         return host.name;
       });
-      console.log('these are the hosts themselves', hosts);
-      $.each(hosts, function(index, host) {
-        $("#host-options")
-          .append('<label><input class="host" type="checkbox" value=' + host + '>  ' + host + '</label>');
-      })
+      console.log('these are the current hosts', hosts);
+      printHosts(hosts);
     },
     error: function(error) {
       console.log('there was an error in the get-hosts call', error);
+      $("#host-options")
+        .html('<h2>There was an error in the initial host call. Please toggle the error code and try again.</h2>');
     }
   });
 
   $('#search-btn').click(function() {
-    console.log( )
-  });
-
-  $("#submit").click(function() {
-    var selectedHosts = $.map($(".host:checked"), function(host) {
-      return host.value;
+    var searchParam = $('#search-field').val();
+    var filtered = hosts.filter(function(host) {
+      return host.match(searchParam);
     });
-    return console.log('User selected the following hosts:', selectedHosts); 
+    if (filtered.length) {
+      $('#host-options').empty();
+      printHosts(filtered);
+    } else {
+      $('#host-options').empty();
+      $('#host-options').html('<h2>There were no hosts that meet your search parameter. Please try again.</h2>');
+    }
   });
 
-  $('.host-options').click(function(){
-    console.log('checkbox working');
-    if($(this).prop("checked")){
-      console.log("Checkbox prop is checked", this.value);
-    }
-    if($(this).is(':checked')) {
-      console.log('Checkbox is checked', this.value);
-    }
+  $('#add-random-host').click(function(){
+    var newHost = Mock.getRandomHost();
+    console.log('newHost is', newHost);
+    hosts.push(newHost.id);
+    $("#host-options")
+        .append('<label><input class="host" type="checkbox" value=' + newHost.id + '>  ' + newHost.name + '</label>');
   });
+
+  $('.host').hover(
+    function() {
+      $(this).append($('<a href="#" class="remove-tag"><i class="remove glyphicon glyphicon-remove"></i></a>'));
+    }
+  );
+
+  $('#host-options i').click(function() {
+    console.log('this remove tag was clicked', $(this)); 
+  })
 
   $("#sort-select").click(function() {
     $.each($(".host"), function(index, host) {
@@ -58,12 +74,8 @@ $(document).ready(function() {
       var x = a.toLowerCase(), y = b.toLowerCase();
       return x < y ? -1 : x > y ? 1 : 0;
     })
-
     $('#host-options').empty();
-    $.each(alphaHosts, function(index, host) {
-      $("#host-options")
-        .append('<label><input class="host" type="checkbox" value=' + host + '>  ' + host + '</label>');
-    })
+    printHosts(alphaHosts);
   });
 
 
@@ -74,14 +86,15 @@ $(document).ready(function() {
     })
 
     $('#host-options').empty();
-    $.each(rAlphaHosts, function(index, host) {
-      $("#host-options")
-        .append('<label><input class="host" type="checkbox" value=' + host + '>  ' + host + '</label>');
-    })
+    printHosts(rAlphaHosts);
   });
 
-  $("#filter-button").click(function() {
-    console.log('dropdown button clicked');
+  $("#submit").click(function() {
+    var selectedHosts = $.map($(".host:checked"), function(host) {
+      return host.value;
+    });
+    return console.log('User selected the following hosts:', selectedHosts); 
   });
+
 
 });
